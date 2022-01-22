8a773f2c269ba111b2b31d8191238e4373127978
6210612823 <6210612823@student.tu.ac.th>
2022-01-23 02:11:59 +0700

    Update main.java

--- main.java
+++ main.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) throws Exception {
         Scanner myObj = new Scanner(System.in); // Create a Scanner object
-        System.out.println("How many object in shapelist.csv ? (Please type number
-         down below)");
+        System.out.println("How many object in shapelist.csv ? (Please type number down below)");
         int shapeNum = myObj.nextInt();
         myObj.close();
 
-        int shapeNum = 4;
+        //int shapeNum = 4;
         String[][] data = read.readShapelist(shapeNum);
         data = read.format(data, shapeNum);
-
-        //for (int i = 0; i < 7; i++) {
-        //    System.out.println(data[3][i]);
-        //}
         
         identify.classify(shapeNum, data);
     }
